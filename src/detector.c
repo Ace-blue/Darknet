b6578c9598f5ba69b582e13af7cb4cb56fa1f9cb
Ace-blue <71758309+Ace-blue@users.noreply.github.com>
2023-06-16 19:07:35 +0800

    Update detector.c

--- src/detector.c
+++ src/detector.c
@@ -336,7 +336,6 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
             fprintf(fid,"\"map\": -1,");
             fprintf(fid,"\"best\": -1,");
         }
-        if (iteration_now>
         fprintf(fid,"\"training_start_time\": \"%0.0f\",",start_time);
         fprintf(fid,"\"time_now\": \"%0.0f\",",what_time_is_it_now());
         // fprintf(fid,"\"map\": %0.2f,",mean_average_precision);
